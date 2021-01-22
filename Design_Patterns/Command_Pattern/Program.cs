3c2fe50e7f5d8e71f6971628ae18ad1ce988e772
xognsrla0102 <xognsrla0102@gmail.com>
2021-01-22 14:40:46 +0900

    초기화

---
+++ Design_Patterns/Command_Pattern/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Command_Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InputHandler inputHandler = new InputHandler();
+
+            inputHandler.HandleInput(BUTTON_KEY.BUTTON_A);
+            inputHandler.HandleInput(BUTTON_KEY.BUTTON_B);
+            inputHandler.HandleInput(BUTTON_KEY.BUTTON_X);
+            inputHandler.HandleInput(BUTTON_KEY.BUTTON_Y);
+        }
+    }
+}
